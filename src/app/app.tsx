8cf94842dc03b8d87bf426e1de6564cec151e115
deux <deux@deuxs-MacBook-Pro.local>
2024-08-01 19:04:23 +0300

    feat: included the i18n in the project

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,6 +2,7 @@ import { Loaders } from './_loaders';
 import { Providers } from './_providers';
 import { Routes } from './_routes';
 import './_styles/globals.css';
+import './_configs';
 
 export const App = () => {
   return (
