a4dca78ccda3e44af56b14124b646db04ff4420d
deux <deux@deuxs-MacBook-Pro.local>
2024-08-01 20:35:41 +0300

    feat: added error-boundary, redirect for unknowns routes

--- src/app/_routes/index.tsx
+++ src/app/_routes/index.tsx
@@ -1,15 +1,21 @@
-import { PageLoader } from '@/shared/ui/page-loader';
-import { HomePageLazy } from '@/pages/home-page';
-import { Route, Switch } from 'wouter';
-import { Suspense } from 'react';
 import { MainLayout } from '@/pages/main-layout/ui/main-layout';
+import { PageLoader } from '@/shared/ui/page-loader';
+import { ROUTES } from '@/shared/constants/routes';
+import { HomePageLazy } from '@/pages/home-page';
+import { Redirect, Route, Switch } from 'wouter';
+import { Suspense } from 'react';
 
 export const Routes = () => {
   return (
     <Suspense fallback={<PageLoader />}>
       <MainLayout>
         <Switch>
-          <Route path="/" component={HomePageLazy} />
+          <Route path={ROUTES.HOME} component={HomePageLazy} />
+
+          <Route>
+            <Redirect to={ROUTES.HOME} />
+          </Route>
+
         </Switch>
       </MainLayout>
     </Suspense>
