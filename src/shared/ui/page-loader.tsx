e182a45bcbf392ccd2dabc839222f235139790e7
deux <deux@deuxs-MacBook-Pro.local>
2024-08-01 10:20:02 +0300

    feat: add prettier, eslint config, github-actions.yml, format script

--- src/shared/ui/page-loader.tsx
+++ src/shared/ui/page-loader.tsx
@@ -1,6 +1,3 @@
-
 export const PageLoader = () => {
-  return (
-    <div>Loading...</div>
-  )
-}
+  return <div>Loading...</div>;
+};
--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,11 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-  ],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   },
-  publicDir: './public',
+  publicDir: './public'
 });
